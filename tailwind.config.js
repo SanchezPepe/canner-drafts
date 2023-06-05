3523c9d42ff46cbe0be7ac3ce399f8f41a91bb35
Jose Sanchez <sanchezpepe97@gmail.com>
2023-06-04 20:17:42 -0600

    added flowbite lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-  content: ["*.vue"],
+  plugins: [require("flowbite/plugin")],
+  content: [
+    "*.vue",
+    "./node_modules/flowbite.{js,ts}",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
 };
