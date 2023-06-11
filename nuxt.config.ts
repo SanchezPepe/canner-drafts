56572139c0e2dc072290cb02d9713d24020ccf37
Jose Sanchez <sanchezpepe97@gmail.com>
2023-06-11 13:51:47 -0600

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 import { NuxtConfig } from "@nuxt/types";
 
 const config: NuxtConfig = {
-  head: {
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["~/assets/css/tailwind.css"],
 };
