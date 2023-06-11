d960358af1a1e7468382f025d1665e2a06d4e0f9
Jose Sanchez <sanchezpepe97@gmail.com>
2023-06-11 15:39:38 -0600

    redirect route on default path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ import { NuxtConfig } from "@nuxt/types";
 const config: NuxtConfig = {
   modules: ["@nuxtjs/tailwindcss"],
   css: ["~/assets/css/tailwind.css"],
+
+  routeRules: {
+    "/": { redirect: "/XYZ" },
+  },
 };
 
 export default config;
