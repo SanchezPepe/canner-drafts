80845a08f258dfdfa34b0f96460d5d802c42ec16
Jose Sanchez <sanchezpepe97@gmail.com>
2025-01-05 20:41:25 -0600

    Updated firebase configs

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 // import any other services you are using like "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyb22pOFhVPNOR6z1nvk7m6eo43I7VxVM",
-  authDomain: "canner-jjsa.firebaseapp.com",
-  projectId: "canner-jjsa",
-  storageBucket: "canner-jjsa.appspot.com",
-  messagingSenderId: "222426529343",
-  appId: "1:222426529343:web:4c41f1edfcff235dccc28a",
+  apiKey: "AIzaSyCDEos25vQOYuGJG6p8HYvGp9Gf3BeLhDI",
+  authDomain: "canner-ppsa.firebaseapp.com",
+  projectId: "canner-ppsa",
+  storageBucket: "canner-ppsa.firebasestorage.app",
+  messagingSenderId: "564917454809",
+  appId: "1:564917454809:web:00d8919bce1db95f017bc8",
 };
 
 // Initialize Firebase
